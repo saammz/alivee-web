c79714cc1d75b93e5bcca4c5ea77363ed8dc9425
CletsyMedia <Cletussam12@yahoo.com>
2024-06-10 16:16:54 +0100

    added resource dir

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Intro = () => {
   return (
     <div>
-      <h1 className='text-7xl text-blue-700'>HELLo DEV</h1>
+      <h1 className='text-7xl text-blue-700'>HELLO DEV</h1>
     </div>
   )
 }
