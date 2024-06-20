070e9e3bdc143a2602c18560215fb69694203834
CletsyMedia <Cletussam12@yahoo.com>
2024-06-20 16:50:27 +0100

    More update on landing page

--- src/components/DarkBG/Premium.jsx
+++ src/components/DarkBG/Premium.jsx
@@ -1,72 +1,328 @@
-import React from 'react';
-
+import { useState } from "react";
 const Premium = () => {
+  const [showYearly, setShowYearly] = useState(false);
+
   return (
-    <div>
-      <div className="plans-green h-full rounded-2xl p-8 bg-center bg-cover bg-green-dark hover:bg-[#90d7a622] border-[#4cc273] border-2">
-        <div className="flex flex-col gap-8 text-white">
-          <div className="flex items-center justify-between">
-            <h3 className="text-4xl leading-32 md:text-40 md:leading-40 font-semibold text-[#4cc273] ">Premium</h3>
-            <div className="flex items-center">
-              <button
-                type="button"
-                className="px-4 py-2 border border-green rounded-md bg-[#87d1a06f] text-white"
-              >
-                <p className="text-12 leading-12 font-medium capitalize">yearly</p>
-              </button>
+      <div className="w-[370px] xsm-max:w-[300px]">
+        <div className="plans-green w-full h-full rounded-2xl py-8 px-6 bg-center bg-cover bg-green-dark hover:bg-[#90d7a622] border-[#4cc273] border-2">
+          <div className="flex flex-col gap-8 text-white">
+            <div className="flex items-center justify-between gap-4">
+              <h3 className="text-4xl leading-32 md:text-40 md:leading-40 font-semibold text-[#4cc273] sm-max:text-2xl xsm-max:text-md">
+                Premium
+              </h3>
+              <div className="flex items-center">
+                <button
+                  type="button"
+                  className={`px-4 py-2 border border-[#4cc273] rounded-l-md ${
+                    !showYearly ? "bg-[#87d1a06f] text-white" : ""
+                  }`}
+                  onClick={() => setShowYearly(false)}
+                >
+                  <p className="text-12 leading-12 font-medium capitalize xsm-max:text-[12px]">
+                    monthly
+                  </p>
+                </button>
+                <button
+                  type="button"
+                  className={`px-4 py-2 border border-green rounded-r-md ${
+                    showYearly ? "bg-[#87d1a06f] text-white" : ""
+                  }`}
+                  onClick={() => setShowYearly(true)}
+                >
+                  <p className="text-12 leading-12 font-medium capitalize xsm-max:text-[12px]">
+                    yearly
+                  </p>
+                </button>
+              </div>
             </div>
+            <div className="flex gap-4 items-baseline mt-2">
+              <h3 className="text-4xl leading-40 md:text-48 md:leading-48 font-semibold">
+                50,15€ <span className="text-base line-through">59,00€</span>
+              </h3>
+              <h4 className="text-16 leading-16 md:text-18 md:leading-18 font-medium">
+                /{showYearly ? "yearly" : "monthly"}
+              </h4>
+            </div>
+            <p className="text-16 leading-16 md:text-18 md:leading-18 font-medium mt-2">
+              Use promo code <span className="text-green-light">EKYEAR50</span>
+            </p>
           </div>
-          <div className="flex gap-4 items-baseline mt-2">
-            <h3 className="text-4xl leading-40 md:text-48 md:leading-48 font-bold">219,99€</h3>
-            <h4 className="text-16 leading-16 md:text-18 md:leading-18 font-medium">/Yearly</h4>
-          </div>
+          {showYearly ? (
+            <ul className="flex flex-col gap-4">
+              <li className="flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-[#4cc273]"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <p className="text-12 leading-12 font-medium text-white">
+                  Personalized plans
+                </p>
+              </li>
+              <li className="flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-[#4cc273]"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <p className="text-12 leading-12 font-medium text-white">
+                  Activities tracking
+                </p>
+              </li>
+              <li className="flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-[#4cc273]"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <p className="text-12 leading-12 font-medium text-white">
+                  Access to exclusive content
+                </p>
+              </li>
+              <li className="flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-[#4cc273]"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <p className="text-12 leading-12 font-medium text-white">
+                  Access to programs
+                </p>
+              </li>
+              <li className="flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-[#4cc273]"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <p className="text-12 leading-12 font-medium text-white">
+                  Access to challenges
+                </p>
+              </li>
+              <li className="flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-[#4cc273]"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <p className="text-12 leading-12 font-medium text-white">
+                  Sync with your wearable
+                </p>
+              </li>
+              <li className="flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-[#4cc273]"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <p className="text-12 leading-12 font-medium text-white">
+                  Sync with your wearable
+                </p>
+              </li>
+            </ul>
+          ) : (
+            <ul className="flex flex-col gap-4">
+              {/* Default monthly options */}
+              <li className="flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-[#4cc273]"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <p className="text-12 leading-12 font-medium text-white">
+                  Personalized plans
+                </p>
+              </li>
+              <li className="flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-[#4cc273]"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <p className="text-12 leading-12 font-medium text-white">
+                  Actives tracking
+                </p>
+              </li>
+              <li className="flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-[#4cc273]"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <p className="text-12 leading-12 font-medium text-white">
+                  Access to exclusive content
+                </p>
+              </li>
+              <li className="flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-[#4cc273]"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <p className="text-12 leading-12 font-medium text-white">
+                  Access to programs
+                </p>
+              </li>
+              <li className="flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-[#4cc273]"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <p className="text-12 leading-12 font-medium text-white">
+                  Access to challenges
+                </p>
+              </li>
+              <li className="flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-[#4cc273]"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <p className="text-12 leading-12 font-medium text-white">
+                  Sync with your wearable
+                </p>
+              </li>
+              <li className="flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-[#4cc273]"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <p className="text-12 leading-12 font-medium text-gray-500">
+                  Sync with your wearable
+                </p>
+              </li>
+            </ul>
+          )}
         </div>
-        <ul className="flex flex-col gap-4 mt-6">
-          <li className="flex items-center gap-2">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-[#4cc273]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
-            </svg>
-            <p className="text-12 leading-12 font-medium text-white">Personalized plans</p>
-          </li>
-          <li className="flex items-center gap-2">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-[#4cc273]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
-            </svg>
-            <p className="text-12 leading-12 font-medium text-white">Activities tracking</p>
-          </li>
-          <li className="flex items-center gap-2">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-[#4cc273]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
-            </svg>
-            <p className="text-12 leading-12 font-medium text-white">Access to exclusive content</p>
-          </li>
-          <li className="flex items-center gap-2">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-[#4cc273]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
-            </svg>
-            <p className="text-12 leading-12 font-medium text-white">Access to programs</p>
-          </li>
-          <li className="flex items-center gap-2">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-[#4cc273]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
-            </svg>
-            <p className="text-12 leading-12 font-medium text-white">Access to challenges</p>
-          </li>
-          <li className="flex items-center gap-2">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-[#4cc273]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
-            </svg>
-            <p className="text-12 leading-12 font-medium text-white">Sync with your wearable</p>
-          </li>
-          <li className="flex items-center gap-2">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-[#4cc273]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
-            </svg>
-            <p className="text-12 leading-12 font-medium text-white">Sync with your wearable</p>
-          </li>
-        </ul>
       </div>
-    </div>
   );
 };
 
