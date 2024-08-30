66b24506358ff5eb9d8fc3af2254115ec5c9e02e
GodfreySam <godfreysam09@gmail.com>
2024-08-30 20:04:44 +0100

    fitbit test branch

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,19 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
 import { Footer } from './components/Footer/Footer';
+import FitbitApiTest from './pages/FitbitTest';
 
 const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Footer />
-    </Router>
-  );
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/fitbit-test" element={<FitbitApiTest />} />
+				</Routes>
+				<Footer />
+			</Router>
+		);
 }
 
 export default App;
